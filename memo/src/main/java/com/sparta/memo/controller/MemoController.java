8f895c71994f49aa80742ec1aa364a6f7151f736
gusrb8925 <gusrb8925@naver.com>
2021-10-24 17:32:06 +0900

    21-10-24

--- memo/src/main/java/com/sparta/memo/controller/MemoController.java
+++ memo/src/main/java/com/sparta/memo/controller/MemoController.java
@@ -7,6 +7,7 @@ import com.sparta.memo.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -16,15 +17,22 @@ public class MemoController {
     private final MemoRepository memoRepository;
     private final MemoService memoService;
 
+//    @GetMapping("/api/memos")
+//    public List<Memo> getMemos() {
+//        return memoRepository.findAllByOrderByModifiedAtDesc();
+//    }
+
+    @GetMapping("/api/memos")
+    public List<Memo> getMemos() {
+        LocalDateTime start = LocalDateTime.now().minusDays(1);
+        LocalDateTime end = LocalDateTime.now();
+        return memoRepository.findAllByModifiedAtBetweenOrderByModifiedAtDesc(start, end);
+    }
+
     @PostMapping("/api/memos")
     public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
         Memo memo = new Memo(requestDto);
         return memoRepository.save(memo);
-    }
-
-    @GetMapping("/api/memos")
-    public List<Memo> getMemos() {
-        return memoRepository.findAllByOrderByModifiedAtDesc();
     }
 
     @PutMapping("/api/memos/{id}")
--- memo/src/main/java/com/sparta/memo/domain/MemoRepository.java
+++ memo/src/main/java/com/sparta/memo/domain/MemoRepository.java
@@ -1,10 +1,14 @@
 package com.sparta.memo.domain;
 
-import com.sparta.memo.domain.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-    List<Memo> findAllByOrderByModifiedAtDesc(); // modifiedAt 기준으로 최신순 정렬(내림차순)
+    // modifiedAt 기준으로 최신순 정렬(내림차순)
+    // List<Memo> findAllByOrderByModifiedAtDesc();
+
+    // 조회 시간으로부터 24시간 이내
+    List<Memo> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime start, LocalDateTime end);
 }
