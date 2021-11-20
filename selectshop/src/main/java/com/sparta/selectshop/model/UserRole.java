eb402c2c4d55a430ff66d436f1b55ad3bc739b65
gusrb8925 <gusrb8925@naver.com>
2021-11-20 16:07:09 +0900

    mv selectshop

--- selectshop/src/main/java/com/sparta/selectshop/model/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.selectshop.model;
-
-public enum UserRole {
-    USER,  // 사용자 권한
-    ADMIN  // 관리자 권한
-}
--- selectshop/src/main/java/com/sparta/selectshop/repository/UserTimeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.selectshop.repository;
-
-import com.sparta.selectshop.model.User;
-import com.sparta.selectshop.model.UserTime;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTimeRepository extends JpaRepository<UserTime, Long> {
-    UserTime findByUser(User user);
-}
